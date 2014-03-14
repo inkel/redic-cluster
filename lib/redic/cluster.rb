8024aa00d0d6282ba3a20f6b05e50c4d10963687
Leandro López (inkel) <inkel.ar@gmail.com>
2014-03-14 13:29:39 -0300

    Support cluster redirection
    Add basic redirection when using Redic against a cluster and the key is
    located in a different node than the current.

---
+++ lib/redic/cluster.rb
@@ -0,0 +1,36 @@
+require "redic"
+
+class Redic::Cluster
+  attr :url
+  attr :node
+  attr_accessor :debug
+
+  def initialize(url="redis://localhost:12001", timeout=10_000_000)
+    @url   = url
+    @node  = Redic::Client.new(url, timeout)
+    @debug = false
+  end
+
+  def call(*args)
+    res = @node.connect do
+      @node.write(args)
+      @node.read
+    end
+
+    return res unless res.is_a?(RuntimeError)
+
+    parts = res.message.split
+
+    if parts.first == "MOVED"
+      slot, addr = parts[1,2]
+
+      $stderr.puts "-> Redirected to slot [#{slot}] located at #{addr}" if @debug
+
+      @node = Redic::Client.new("redis://#{addr}", @node.timeout)
+
+      call(*args)
+    else
+      res
+    end
+  end
+end
